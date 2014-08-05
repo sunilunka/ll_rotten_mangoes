1bd0c58e8388cdd2abb3bbb14c368e3090bc7e7f
Sunil Unka <sunil.unka@gmail.com>
2014-08-05 19:19:01 +0000

    Completed step 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,5 +16,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, :notice "Adios!"
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   
   # get 'movies/index' to: 'movies#index'
 
