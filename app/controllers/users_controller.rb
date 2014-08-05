7f71209dee1e7580c473cb751ee255480287c047
Sunil Unka <sunil.unka@gmail.com>
2014-08-05 18:52:45 +0000

    Updated User controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = user.id 
       redirect_to movies_path
     else
       render :new
