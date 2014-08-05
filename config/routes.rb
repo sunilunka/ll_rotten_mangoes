736bb3a0062bebd21848637c8b3defc64b05892d
Sunil Unka <sunil.unka@gmail.com>
2014-08-05 21:26:55 +0000

    Completed all mandatory parts of Rotten Mangoes exercise.

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
+
+  root to: 'movies#index'
   
   # get 'movies/index' to: 'movies#index'
 
