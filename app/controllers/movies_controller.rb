3b7bf324ff7880b6d9447cef900c0b0462415415
Sunil Unka <sunil.unka@gmail.com>
2014-08-05 17:45:01 +0000

    Completed step 3.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,58 @@
 class MoviesController < ApplicationController
-  def index
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(
+      title: params[:movie][:title],
+      director: params[:movie][:director],
+      runtime_in_minutes: params[:movie][:runtime_in_minutes],
+      description: params[:movie][:description],
+      poster_image_url: params[:movie][:poster_image_url],
+      release_date: params[:movie][:release_date]  
+    )
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def index 
+    @movies =  Movie.all
   end
 
   def show
-  end
-
-  def new
+    @movie = Movie.find(params[:id])
   end
 
   def edit
+    @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update_attributes(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  protected
+
+  def movie_params
+    params.require(:movie).permit(:title, :director, :runtime_in_minutes, :description, :poster_image_url, :release_date)
+  end
+
 end
