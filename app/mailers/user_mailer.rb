70d3e7327b0a923be8406a6320a0e1ed83387f1c
Sunil Unka <sunil.unka@gmail.com>
2014-08-07 21:01:01 +0000

    Added email templates.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "admin@rottenmangoes.com"
 
-  def email_removed_user
+  def email_removed_user(user)
     @user = current_user
     @url = www.rottenmangoes.com/contact_us
     mailto: (to: @user.email, subject: "You have been removed from Rotten Mangoes.")
