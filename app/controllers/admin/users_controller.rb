7138e63534eebf8cfa662c6132e3228e0eb78f1d
Sunil Unka <sunil.unka@gmail.com>
2014-08-08 18:51:03 +0000

    Working on edit and creating a new user.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,8 +6,36 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def new 
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+  
+    if @user.save
+       redirect_to admin_user_path, notice: "New user with email created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -15,6 +43,12 @@ class Admin::UsersController < ApplicationController
     UserMailer.email_removed_user(@user).deliver
     @user.destroy
     redirect_to admin_users_path
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
   end
 
 
