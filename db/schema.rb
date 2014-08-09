84573a1ebc17d6ce95fb2ae0eaa6d7aa562cda21
Sunil Unka <sunil.unka@gmail.com>
2014-08-09 00:00:47 +0000

    Added another file to the commit.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806164233) do
+ActiveRecord::Schema.define(version: 20140808204505) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20140806164233) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
-    t.boolean  "is_admin"
+    t.integer  "is_admin"
   end
 
 end
